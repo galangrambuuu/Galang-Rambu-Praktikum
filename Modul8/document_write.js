9ed07f15872178aec8fdc7e3c51af3b9b668f289
galang rambu anarki <galangrambu008@gmail.com>
2026-01-11 23:52:33 +0700

    Menambahkan folder Modul8

---
+++ Modul8/document_write.js
@@ -0,0 +1,17 @@
+<HTML>
+<HEAD>
+<TITLE>Skrip JavaScript</TITLE>
+</HEAD>
+<BODY>
+
+Percobaan memakai JavaScript:
+
+<SCRIPT LANGUAGE="JavaScript">
+
+document.write("Selamat Mencoba JavaScript<br>");
+document.write("Semoga sukses!");
+   </br>
+</SCRIPT>
+
+</BODY>
+</HTML>
